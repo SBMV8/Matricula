5bfa76150b2419de7661c3fe934b4efda841aed3
Shirley Martínez <97803763+SBMV8@users.noreply.github.com>
2024-10-28 12:15:33 -0500

    first commit

--- FIIS/settings.py
+++ FIIS/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'FIIS.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/posgres',
         conn_max_age=600
     )
 }
